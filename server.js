958872f036f51a0dcf1d178bba79055b85f2ea57
Balotelli99 <sultanavdi06@gmail.com>
2026-04-10 14:15:36 +0200

    alleen nog online zetten

--- server.js
+++ server.js
@@ -7,29 +7,70 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-// Serve specific folders statically so Express properly sends CSS and other assets
+const connectedUsers = new Map();
+
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/html', express.static(path.join(__dirname, 'html')));
 
-// Ensure root URL goes to voorkant.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'html', 'voorkant.html'));
 });
+
+function broadcastUserList() {
+    const userList = Array.from(connectedUsers.values()).map(u => ({ username: u.username }));
+    const message = JSON.stringify({ type: 'userList', users: userList });
+    wss.clients.forEach((client) => {
+        if (client.readyState === 1) {
+            client.send(message);
+        }
+    });
+    console.log('User list broadcast:', userList);
+}
+
+function sendPrivateMessage(senderWs, targetUsername, content) {
+    let targetWs = null;
+    for (const [ws, user] of connectedUsers.entries()) {
+        if (user.username === targetUsername) {
+            targetWs = ws;
+            break;
+        }
+    }
+    
+    if (targetWs && targetWs.readyState === 1) {
+        const senderName = connectedUsers.get(senderWs).username;
+        const message = JSON.stringify({
+            type: 'privateMessage',
+            from: senderName,
+            content: content
+        });
+        targetWs.send(message);
+        console.log(`Private message from ${senderName} to ${targetUsername}: ${content}`);
+    }
+}
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
 
     ws.on('message', (message) => {
-        console.log('Received:', message.toString());
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === 1) {
-                client.send(message.toString());
-            }
-        });
+        const data = JSON.parse(message.toString());
+        console.log('Received:', data);
+        
+        if (data.type === 'login') {
+            connectedUsers.set(ws, { username: data.username });
+            console.log(`User logged in: ${data.username}`);
+            broadcastUserList();
+        } else if (data.type === 'privateMessage') {
+            sendPrivateMessage(ws, data.to, data.content);
+        }
     });
 
     ws.on('close', () => {
-        console.log('Client disconnected');
+        const user = connectedUsers.get(ws);
+        if (user) {
+            console.log(`User disconnected: ${user.username}`);
+            connectedUsers.delete(ws);
+            broadcastUserList();
+        }
     });
 });
 
